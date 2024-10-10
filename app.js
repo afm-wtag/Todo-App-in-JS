6dcf75f5017ccaf7979eeb78ca1b274de87e1673
Afnan Alauddin Mumu <afnan.mumu@welldev.io>
2024-10-10 13:56:18 +0600

    Adding in Todo implemented

---
+++ app.js
@@ -0,0 +1,21 @@
+const taskManager = () => {
+  const inputTodo = document.getElementById("inputTodo");
+  const addTodoButton = document.getElementById("addTodoButton");
+  const listTodo = document.getElementById("listTodo");
+  const addTodo = () => {
+    const inputText = inputTodo.value.trim();
+    if (inputText == "") {
+      alert("Enter Something!");
+      return;
+    }
+    listTodo.appendChild(addNewTask(inputText));
+    inputTodo.value = "";
+  };
+  const addNewTask = (text) => {
+    const li = document.createElement("li");
+    li.innerText = text;
+    return li;
+  };
+  addTodoButton.addEventListener("click", addTodo);
+};
+document.addEventListener("DOMContentLoaded", taskManager);
