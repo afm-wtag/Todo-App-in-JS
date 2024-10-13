0362dbb96346ec2ca4b1ffa7e9c56c20f21d6d64
Afnan Alauddin Mumu <afnan.mumu@welldev.io>
2024-10-13 10:27:53 +0600

    Updated the code according to code review

--- app.js
+++ app.js
@@ -1,21 +1,51 @@
 const taskManager = () => {
+  let todos = [];
   const $todoInput = document.getElementById("inputTodo");
   const $addTodoButton = document.getElementById("addTodoButton");
   const $todoList = document.getElementById("listTodo");
-  const addTodo = () => {
-    const inputText = inputTodo.value.trim();
+  const $feedbackMessage = document.getElementById("feedbackMessage");
+  function renderTodos() {
+    // iterate over the todos list, create specific todos, and insert them in the DOM
+    $todoList.innerHTML = "";
+    todos.forEach((todo) => {
+      const todoElement = createTodo(todo);
+      $todoList.appendChild(todoElement);
+    });
+  }
+  function createTodo(todo) {
+    // responsible for creating a todo element based on the todo object
+    const li = document.createElement("li");
+    const todoText = document.createElement("span");
+    todoText.textContent = todo.title;
+    li.appendChild(todoText);
+    return li;
+  }
+  function showFeeback() {
+    $feedbackMessage.innerText = "Please Enter a valid Todo item";
+    $feedbackMessage.style.color = "red";
+    $feedbackMessage.style.fontSize = "12px";
+  }
+  function clearFeedback() {
+    $feedbackMessage.innerText = "";
+  }
+  function onAddTodo() {
+    // responsible for delegating the work needed to create a todo object
+    // for example,
+    const inputText = $todoInput.value.trim();
     if (inputText == "") {
-      alert("Enter Something!");
+      showFeeback();
       return;
     }
-    listTodo.appendChild(addNewTask(inputText));
-    inputTodo.value = "";
-  };
-  const addNewTask = (text) => {
-    const li = document.createElement("li");
-    li.innerText = text;
-    return li;
-  };
-  addTodoButton.addEventListener("click", addTodo);
+    clearFeedback();
+    const todo = {
+      title: $todoInput.value,
+      createdAt: Date.now(),
+      //... other options might be added in the future
+    };
+    todos.push(todo);
+    renderTodos();
+    $todoInput.value = "";
+  }
+  $addTodoButton.addEventListener("click", onAddTodo);
 };
 document.addEventListener("DOMContentLoaded", taskManager);
